d6a1b974014b9234ee9df0369fb6f536f9b23957
cong-bang.do <docongbang1993@gmail.com>
2020-02-20 21:32:53 +0100

    Remove test components

--- src/main.js
+++ src/main.js
@@ -1,13 +1,5 @@
-import LikeButton from './components/like_button';
-import One from './components/one';
-import LoveButton from './components/love_button';
 import SimpleTable from './components/table';
-import Button from './components/button';
 
 export {
-  LikeButton,
-  LoveButton,
-  One,
-  SimpleTable,
-  Button
+  SimpleTable
 };
